7e630ec71757e6820ecaae8b8464fc6d4c9c9490
Alysonviana <alyson.viana@iesb.edu.br>
2026-03-19 21:44:30 -0300

    fear: pratica-02 projeto-chronos-pomodoro

---
+++ praticas/ativ_praticas/projeto_react/chronos-pomodoro/src/App.tsx
@@ -0,0 +1,9 @@
+export function App() {
+  return (
+    <div>
+      <h1>Bem-vindo a primeira aula jsx</h1>
+      <h2>nossa aula de introdução</h2>
+    </div>
+  );
+}
+
---
+++ praticas/ativ_praticas/projeto_react/chronos-pomodoro/src/main.tsx
@@ -0,0 +1,8 @@
+import { createRoot } from "react-dom/client";  
+
+createRoot(document.getElementById("root") as HTMLElement).render(
+  <div>
+    <h1>Bem-vindo a primeira aula jsx</h1>
+    <h2>nossa aula de introdução</h2>
+  </div>
+);
